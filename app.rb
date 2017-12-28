c233fca46c65199b80fd19451e9f87b86bec3a66
Skyler Layne <skylerclayne@gmail.com>
2017-12-28 18:32:02 -0500

    render json please
    Signed-off-by: Skyler Layne <skylerclayne@gmail.com>

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ $stdout.sync = true
 require 'sinatra'
 require 'google_assistant'
 require 'json'
+require 'sinatra/json'
 
 class App < Sinatra::Base
   get '/' do
@@ -13,10 +14,20 @@ class App < Sinatra::Base
     content_type :json
     data = JSON.parse(request.body.read)
     assistant_response = GoogleAssistant.respond_to(data, response) do |assistant|
-      assistant.intent.main do
+    assistant.intent.main do
+        assistant.ask(
+          prompt: "<speak>Hi there! Say something, please.</speak>",
+          no_input_prompt: [
+            "<speak>If you said something, I didn't hear you.</speak>",
+            "<speak>Did you say something?</speak>"
+          ]
+        )
+      end
+
+      assistant.intent.text do
         assistant.tell("<speak>I can respond, too!</speak>")
       end
     end
-    assistant_response
+    json assistant_response
   end
 end
