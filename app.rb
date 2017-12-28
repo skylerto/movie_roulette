f14ac03fcaa0db0e1fb3e54ef7e800b345a620db
Skyler Layne <skylerclayne@gmail.com>
2017-12-28 18:06:01 -0500

    no google assistant
    Signed-off-by: Skyler Layne <skylerclayne@gmail.com>

--- app.rb
+++ app.rb
@@ -9,12 +9,12 @@ class App < Sinatra::Base
   post '/' do
     puts params
     puts response
-    assistant_response = GoogleAssistant.respond_to(params, response) do |assistant|
-      assistant.intent.main do
-        assistant.tell("<speak>I can respond, too!</speak>")
-      end
-    end
+    # assistant_response = GoogleAssistant.respond_to(params, response) do |assistant|
+    #   assistant.intent.main do
+    #     assistant.tell("<speak>I can respond, too!</speak>")
+    #   end
+    # end
 
-    render json: assistant_response
+    # render json: assistant_response
   end
 end
