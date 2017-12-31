671d650c29c137eb0f17b63ca6b5a5b632f52b2e
Skyler Layne <skylerclayne@gmail.com>
2017-12-31 17:12:01 -0500

    remove ivar
    Signed-off-by: Skyler Layne <skylerclayne@gmail.com>

--- lib/movie_roulette/commands/tell_me_about.rb
+++ lib/movie_roulette/commands/tell_me_about.rb
@@ -9,7 +9,7 @@ class TellMeAbout
     @assistant.conversation.data['movie'] = movie.title
     method = @assistant.arguments[0].text_value.downcase.gsub(' ', '_')
     if movie.options.include?(method)
-      res = movie.send("@#{method}".to_s)
+      res = movie.send(method.to_s)
       respond_with = "#{res}, how does that sound?"
       @assistant.ask(respond_with, [respond_with])
     else
