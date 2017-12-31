ad689e118edab8dde290f0b9fef565088163dfb7
Skyler Layne <skylerclayne@gmail.com>
2017-12-31 18:32:54 -0500

    debuging more info
    Signed-off-by: Skyler Layne <skylerclayne@gmail.com>

--- lib/movie_roulette/commands/tell_me_about.rb
+++ lib/movie_roulette/commands/tell_me_about.rb
@@ -10,7 +10,8 @@ class TellMeAbout
     movie = Movie.find(title: movie_title)
     method = @assistant.arguments[0].text_value.downcase.gsub(' ', '_')
     if movie.options.include?(method)
-      res = movie.send(method.to_s)
+      puts "Attempting #{method} on #{movie.inspect}"
+      res = movie.send(method.to_sym)
       respond_with = "#{res}, how does that sound?"
       return @assistant.ask(respond_with, [respond_with])
     else
